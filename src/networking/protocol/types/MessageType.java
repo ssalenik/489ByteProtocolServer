91d020482f72583ab37bfc5c2715042bebb74e1e
Stepan Salenikovich <stepan.salenikovich@gmail.com>
2014-03-23 14:44:07 -0400

    added new server message types

--- src/networking/protocol/types/MessageType.java
+++ src/networking/protocol/types/MessageType.java
@@ -10,7 +10,14 @@ public enum MessageType {
 	DELETE_USER(6),
 	CREATE_STORE(7),
 	SEND_TO_USER(8), // <username>,message
-	QUERY_MESSAGES(9);
+	QUERY_MESSAGES(9),
+	REQUEST_SEND_FILE(10), // <username>,<filesize>,<filename>
+	SEND_FILE_CHUNK(11),
+	CANCEL_SEND_FILE(12),
+	REQUEST_RECEIVE_FILE(13), // <username>,<filename>
+	RECEIVE_FILE_CHUNK(14),
+	CANCEL_RECEIVE_FILE(15);
+	
 	
 	private int i;
 	
